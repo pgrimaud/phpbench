0b2bf484348f7f219bbaa999f4360f150fa22988
Pierre Grimaud <grimaud.pierre@gmail.com>
2020-04-03 23:48:16 +0200

    Add first bench test

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# phpbench
+# PHPBench
 Some PHP benchmarks - Very nice! WoW!
+
+Based on [phpbench/phpbench](https://github.com/phpbench/phpbench).
+
+## Installation
+
+```
+composer install
+```
+
+## Usage
+
+Run all tests
+
+```
+bin/bench benchmarks 
+```
+
+Run specific test
+
+```
+bin/bench benchmarks/FirstElementOfArray.php
+```
+
+## LICENSE
+Licensed under the terms of the MIT License.
---
+++ benchmarks/FirstElementOfArray.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * @Revs(1000000)
+ * @Iterations(5)
+ */
+class FirstElementOfArray
+{
+    const TEST_ARRAY = [
+        'A', 'B', 'C',
+    ];
+
+    public function benchWithCurrent()
+    {
+        $array = self::TEST_ARRAY;
+        $x     = current($array);
+    }
+
+    public function benchWithFirstKey()
+    {
+        $array = self::TEST_ARRAY;
+        $x     = $array[0];
+    }
+
+    public function benchWithForeach()
+    {
+        $array = self::TEST_ARRAY;
+
+        foreach ($array as $value) {
+            $x = $value;
+            break;
+        }
+    }
+
+    public function benchWithReset()
+    {
+        $array = self::TEST_ARRAY;
+        $x     = reset($array);
+    }
+}
